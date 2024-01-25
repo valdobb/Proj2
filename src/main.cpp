c7ce4c7f2388b785d82e56d5b7da21b105d4f32c
Karol Macedonski <karolskopje@gmail.com>
2024-01-25 02:31:41 +0100

    2:31 Commit

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,55 @@
-#include <Arduino.h>
+#include <SPI.h>
+#include <Wire.h>
+#include <Adafruit_BMP085.h>
 
-// put function declarations here:
-int myFunction(int, int);
+Adafruit_BMP085 bmp180;
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  Serial.begin(9600);
+  if (!bmp180.begin()) {
+    Serial.println("Could not find a valid BMP085 sensor, check wiring!");
+    
+  }
 }
+
+
+void ReadTemperature() {
+  float temperatureC = bmp180.readTemperature();
+  String TempString = "Temp: " + String(temperatureC, 2) + "°C";
+  Serial.println(TempString);
+}
+
+void ReadPressure() {
+  float Pressure = bmp180.readPressure();
+  String PressureString = "Pressure: " + String(Pressure, 2);
+  Serial.println(PressureString);
+}
+
+void ReadAltitude() {
+  float Altitude = bmp180.readAltitude();
+  String AltitudeString = "Altitude: " + String(Altitude, 2);
+  Serial.println(AltitudeString);
+}
+
+void ReadPressureSeaLevel() {
+  float PressureSeaLevel = bmp180.readSealevelPressure();
+  String PressureSeaLevelString = "PressureSeaLevel: " + String(PressureSeaLevel, 2);
+  Serial.println(PressureSeaLevelString);
+}
+
+void ReadRealAltitude() {
+  float Altitude = bmp180.readAltitude(101500);
+  String AltitudeString = "RealAltitude: " + String(Altitude, 2);
+  Serial.println(AltitudeString);
+}
+
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  ReadTemperature();
+  ReadPressure();
+  ReadAltitude();
+  ReadPressureSeaLevel();
+  ReadRealAltitude();
+  delay(1000); // Adjust the delay based on your application requirements
 }
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
-}
